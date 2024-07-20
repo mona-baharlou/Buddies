cd75e1164c8258666d04515dc18b3011625af477
mona-baharlou <mona.baharlou755@gmail.com>
2024-07-20 15:40:31 +0330

    refactor SignUpViewModel.kt code

--- app/src/main/java/com/baharlou/buddies/signup/SignUpViewModel.kt
+++ app/src/main/java/com/baharlou/buddies/signup/SignUpViewModel.kt
@@ -12,20 +12,43 @@ class SignUpViewModel {
 
     fun createAccount(email: String, password: String) {
 
+        val state = when (validate(email, password)) {
+            is CredentialValidationResult.InvalidEmail ->
+                SignUpState.BadEmail
+
+            is CredentialValidationResult.InvalidPassword ->
+                SignUpState.BadPassword
+        }
+
+        _mutableSignUpState.value = state
+    }
+
+    private fun validate(
+        email: String,
+        password: String,
+    ): CredentialValidationResult {
         val emailRegex =
             "[a-zA-Z0-9+._\\%-+]{1,256}\\@[a-zA-Z0-9][a-zA-Z0-9-]{0,64}(.[a-zA-Z0-9][a-zA-Z0-9-]{0,25})+"
         val emailPattern = Pattern.compile(emailRegex)
 
-
         val passwordRegex = """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
         val passwordPattern = Pattern.compile(passwordRegex)
 
-        if (!emailPattern.matcher(email).matches()) {
-            _mutableSignUpState.value = SignUpState.BadEmail
-        } else if (!passwordPattern.matcher(password).matches()) {
-            _mutableSignUpState.value = SignUpState.BadPassword
 
-        }
+        val result = if (!emailPattern.matcher(email).matches()) {
+            CredentialValidationResult.InvalidEmail
+        } else if (!passwordPattern.matcher(password).matches()) {
+            CredentialValidationResult.InvalidPassword
+
+        } else TODO()
+        return result
+    }
+
+    sealed class CredentialValidationResult {
+        object InvalidEmail : CredentialValidationResult()
+        object InvalidPassword : CredentialValidationResult()
     }
 
 }
+
+

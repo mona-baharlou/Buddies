bbee9ae82640e9950bf0b2698e00f2bcfa60d240
mona-baharlou <mona.baharlou755@gmail.com>
2024-07-20 14:55:11 +0330

    Detecting invalid email formats

--- app/src/test/java/com/baharlou/buddies/signup/CredentialValidationTest.kt
+++ app/src/test/java/com/baharlou/buddies/signup/CredentialValidationTest.kt
@@ -3,14 +3,23 @@ package com.baharlou.buddies.signup
 import com.baharlou.buddies.InstantTaskExecutorExtension
 import com.baharlou.buddies.signup.state.SignUpState
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 @ExtendWith(InstantTaskExecutorExtension::class)
 class CredentialValidationTest {
 
-    @Test
-    fun invalidEmail(){
+    @ParameterizedTest
+    @CsvSource(
+        "'email'",
+        "'a@b.c'",
+        "'ab@b.c'",
+        "'ab@bc.c'",
+        "''",
+        "'   '",
+    )
+    fun invalidEmail(email: String) {
         val viewModel = SignUpViewModel()
         viewModel.createAccount("mona", "password")
         assertEquals(SignUpState.BadEmail, viewModel.signUpState.value)
